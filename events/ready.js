2ec54df15930164df9fcd944a605c51cd3dc8abe
RevokedCookie <hpaulson@smc.wtf>
2019-11-04 18:15:24 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js')
 
 module.exports = (client, message) => {
     client.user.setActivity("PokeCard v0.0.1 - p!search"); 
-    client.channels.get('618020151245144077').send(
+    client.channels.get('CHANNEL_ID').send(
         new Discord.MessageEmbed()
             .setAuthor('PokeCard Startup')
-            .setDescription('<a:check:620394300408332294> **Hello World!** I\m PokeCard, and i\'m a bot developed by <@516840368843522073> to help serve the All Things Pokemon Discord server!')  
+            .setDescription('<a:check:620394300408332294> **Hello World!** I\m PokeCard, and i\'m a bot developed by RevokedCookie to help serve the All Things Pokemon Discord server!')  
             .setColor(0x00d933)
             .setTimestamp())
     console.log('Pokecard has started up')
